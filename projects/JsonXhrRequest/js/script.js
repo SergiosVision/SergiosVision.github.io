6d485fb01a6f1c13d7883984ff3728b3412ff8f0
SergiosVision <30858380+SergiosVision@users.noreply.github.com>
2017-12-01 15:21:25 +0700

    Add if statement

--- projects/JsonXhrRequest/js/script.js
+++ projects/JsonXhrRequest/js/script.js
@@ -4,6 +4,8 @@ var getScrollTopBtn = document.querySelector('.scrollTopBth');
 var getBody = document.body;
 var getBodySecond = document.documentElement;
 
+// Function init Call button disable
+
 function callDisableButton() {
     if (getSearchField.value == '') {
         getResetBtn.setAttribute('disabled', true);
@@ -11,11 +13,13 @@ function callDisableButton() {
         getResetBtn.removeAttribute('disabled');
     }
 }
+callDisableButton(); // Call button disable
 
-callDisableButton();
+// Function init XHR request
 
 function callRequest() {
     var getValueForm = document.querySelector('#search').value;
+    var getUpdateContainer = document.querySelector('#update');
     var expression = new RegExp(getValueForm, 'i');
     var request = new XMLHttpRequest();
     request.open('GET', 'js/data.json');
@@ -23,8 +27,10 @@ function callRequest() {
         if ((request.readyState === 4) && (request.status === 200)) {
             var items = JSON.parse(request.responseText);
             var output = '<ul class="searchResults">';
+            var a = false;
             for (var key in items) {
-                if ((items[key].name.search(expression) != - 1) || (items[key].bio.search(expression) != - 1) || (items[key].reknown.search(expression) != - 1) ) {
+                if ((items[key].name.search(expression) != - 1) || (items[key].bio.search(expression) != - 1) || (items[key].reknown.search(expression) != - 1)) {
+                    a = true;
                     output += '<li class="'+ items[key].name +'">';
                     output += '<h2>'+ items[key].name +'</h2>';
                     output += '<img src="images/'+ items[key].shortname +'_tn.jpg" alt="'+ items[key].name +'"/>';
@@ -35,13 +41,21 @@ function callRequest() {
                 }
             }
             output += '</ul>';
-            var getUpdateContainer = document.querySelector('#update');
-            getUpdateContainer.innerHTML = output;
+            if(a) {
+                getUpdateContainer.innerHTML = output;
+            }
+            else {
+                getUpdateContainer.innerHTML = '<h1>Nothing found on your request</h1>';
+            }
+        } else {
+            getUpdateContainer.innerHTML = '<h1>Connection error. Try again later</h1>';
         }
     };
     request.send();
 }
-callRequest();
+callRequest(); // Call XHR request
+
+// Function init Deleting Links if they empty
 
 function deleteEmptyLinks() {
     setTimeout(function () {
@@ -53,26 +67,26 @@ function deleteEmptyLinks() {
         })
     },1000);
 }
-deleteEmptyLinks();
+deleteEmptyLinks(); // Deleting Links if they empty
 
-getSearchField.addEventListener('keyup', function () {
-    callRequest();
-    deleteEmptyLinks();
-    callDisableButton();
+getSearchField.addEventListener('keyup', function () { // Calling this function when change happen in input keyup
+    callRequest(); // Call XHR request
+    deleteEmptyLinks(); // Deleting Links if they empty
+    callDisableButton(); // Call button disable
 });
 
-getSearchField.addEventListener('change', function () {
-    callRequest();
-    deleteEmptyLinks();
-    callDisableButton();
+getSearchField.addEventListener('change', function () { // Calling this function when change happen in input
+    callRequest(); // Call XHR request
+    deleteEmptyLinks(); // Deleting Links if they empty
+    callDisableButton(); // Call button disable
 });
 
-getResetBtn.addEventListener('click', function () {
+getResetBtn.addEventListener('click', function () { // Reset button
     getSearchField.value = '';
     getSearchField.focus();
-    callRequest();
-    deleteEmptyLinks();
-    callDisableButton();
+    callRequest(); // Call XHR request
+    deleteEmptyLinks(); // Deleting Links if they empty
+    callDisableButton(); // Call button disable
 });
 
 
@@ -80,30 +94,29 @@ getResetBtn.addEventListener('click', function () {
 // Show Scroll button
 
 window.addEventListener('scroll', function () {
-   if (getBody.scrollTop > 200 || getBodySecond.scrollTop > 200) {
+   if (getBody.scrollTop > 200 || getBodySecond.scrollTop > 200) { // Use two body tags for all browsers
        getScrollTopBtn.classList.add('showButton');
    } else {
        getScrollTopBtn.classList.remove('showButton');
    }
 });
 
-
 //Smooth scroll vanilla
 
-var clerThisInterval = 0; // Needed to cancel the scrolling when we're at the top of the page
+var clearThisInterval = 0; // Needed to cancel the scrolling when we're at the top of the page
 
 function scrollStep() {
     // Check if we're at the top already. If so, stop scrolling by clearing the interval
     if (window.pageYOffset === 0) {
-        clearInterval(clerThisInterval);
+        clearInterval(clearThisInterval);
     }
 
     window.scroll(0, window.pageYOffset - 50);
 }
 
 function scrollToTop() {
-    // Call the function scrollStep() every 5 millisecons
-    clerThisInterval = setInterval(scrollStep, 5);
+    // Call the function scrollStep() every 5 milliseconds
+    clearThisInterval = setInterval(scrollStep, 5);
     getScrollTopBtn.setAttribute('disabled', true);
     setTimeout(function () {
         getScrollTopBtn.removeAttribute('disabled');
