00c233149ba622e08682cc8da9c947f5b547977e
SergiosVision <sjubar2012@gmail.com>
2017-12-03 15:55:49 +0700

    Add SmoothScroll

--- projects/FetchRequest/js/script.js
+++ projects/FetchRequest/js/script.js
@@ -107,37 +107,6 @@ function callScrollSpy() {
             }
 
         });
-        // var getSections = document.querySelectorAll('.searchResults li[id]');
-        // var getNavLinkOne = document.querySelector('.navigation ul li a');
-        // var getNavLinks = document.querySelectorAll('.navigation ul li a');
-        // getNavLinkOne.classList.add('active');
-        //
-        // function getClosestSection() {
-        //     var sectionsLength = getSections.length;
-        //     for(var index = 0; index < sectionsLength; index++) {
-        //         if (isBelowScroll(getSections.item(index)))
-        //             break;
-        //     }
-        //     selectLink(getSections.item(index).id)
-        // }
-        //
-        // function isBelowScroll(element) {
-        //     var position = element.getBoundingClientRect();
-        //     return position.top > 0;
-        // }
-        //
-        // function selectLink(id) {
-        //     Array.prototype.forEach.call(getNavLinks, function(element){
-        //         element.classList.remove('active');
-        //     });
-        //
-        //     document.querySelector('.navigation a[href="#'+id+'"]').classList.add('active');
-        // }
-        //
-        // window.addEventListener('scroll', function() {
-        //     getClosestSection();
-        // });
-        
     }, 1000);
 }
 callScrollSpy();
@@ -200,3 +169,82 @@ function scrollToTop() {
 
 // When the DOM is loaded, this click handler is added to our scroll button
 getScrollTopBtn.addEventListener('click',scrollToTop);
+
+
+// Smooth Scroll
+
+setTimeout(function () {
+    // Feature Test
+    if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
+
+        // Function to animate the scroll
+        var smoothScroll = function (anchor, duration) {
+
+            // Calculate how far and how fast to scroll
+            var startLocation = window.pageYOffset;
+            var endLocation = anchor.offsetTop;
+            var distance = endLocation - startLocation;
+            var increments = distance/(duration/16);
+            var stopAnimation;
+
+            // Scroll the page by an increment, and check if it's time to stop
+            var animateScroll = function () {
+                window.scrollBy(0, increments);
+                stopAnimation();
+            };
+
+            // If scrolling down
+            if ( increments >= 0 ) {
+                // Stop animation when you reach the anchor OR the bottom of the page
+                stopAnimation = function () {
+                    var travelled = window.pageYOffset;
+                    if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+                        clearInterval(runAnimation);
+                    }
+                };
+            }
+            // If scrolling up
+            else {
+                // Stop animation when you reach the anchor OR the top of the page
+                stopAnimation = function () {
+                    var travelled = window.pageYOffset;
+                    if ( travelled <= (endLocation || 0) ) {
+                        clearInterval(runAnimation);
+                    }
+                };
+            }
+
+            // Loop the animation function
+            var runAnimation = setInterval(animateScroll, 8);
+
+        };
+
+        // Define smooth scroll links
+        var scrollToggle = document.querySelectorAll('.navigation a');
+
+        // For each smooth scroll link
+        [].forEach.call(scrollToggle, function (toggle) {
+
+            // When the smooth scroll link is clicked
+            toggle.addEventListener('click', function(e) {
+
+                // Prevent the default link behavior
+                e.preventDefault();
+
+                // Get anchor link and calculate distance from the top
+                var dataID = toggle.getAttribute('href');
+                var dataTarget = document.querySelector(dataID);
+                var dataSpeed = toggle.getAttribute('data-speed');
+
+                // If the anchor exists
+                if (dataTarget) {
+                    // Scroll to the anchor
+                    smoothScroll(dataTarget, dataSpeed || 500);
+                }
+
+            }, false);
+
+        });
+
+    }
+}, 1000);
