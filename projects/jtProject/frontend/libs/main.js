d3e2882cc9b59e21ca12cfbd23349cbe19c07aca
SergiosVision <sjubar2012@gmail.com>
2018-01-31 23:12:55 +0700

    Add new fixes

--- projects/jtProject/frontend/libs/main.js
+++ projects/jtProject/frontend/libs/main.js
@@ -1,62 +1,45 @@
 svg4everybody();
+$(function ($) {
+    $.fn.horizontScrl = function (select, amount) {
+        var cmp = $(this);
+        amount = amount || 120;
+        cmp.css('overflow', 'auto');
+        $(select).bind("DOMMouseScroll mousewheel", function (event) {
+            var oEvent = event.originalEvent,
+                direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta,
+                position = cmp.scrollLeft();
+            position += direction > 0 ? -amount : amount;
+            cmp.scrollLeft(position);
+            event.preventDefault();
+        })
+    };
+});
+
+$.fn.scrollEnd = function(callback, timeout) {
+    $(this).scroll(function(){
+        var $this = $(this);
+        if ($this.data('scrollTimeout')) {
+            clearTimeout($this.data('scrollTimeout'));
+        }
+        $this.data('scrollTimeout', setTimeout(callback,timeout));
+    });
+};
 
 $(document).ready(function () {
     // jQuery('.t-scrollContainer').scrollbar();
+    if(window.matchMedia('(min-width: 768px)').matches) {
+        $('.t-mainCardsHolder').horizontScrl('body', 150); // Вводим скорость горизонтального скролла а также зону действия скролла
+        $('.t-mainCardsHolder').on('scroll', function () {
+            if ($('.t-mainCardsHolder .t-newsCardWrapper').css("marginLeft") >= 35) {
+                return false
+            } else {
+                $('.t-mainCardsHolder .t-newsCardWrapper:not(:last-child)').css('margin-right', '35px');
+            }
+        });
 
-    function Lim() {
-        var pos = $('.t-newsCardWrapper').last().position().left + $('t-newsCardWrapper').last().width() - $(window).width() - 50;
-        return pos;
+        $('.t-mainCardsHolder').scrollEnd(function(){
+            $('.t-newsCardWrapper').attr('style', '');
+        }, 300);
     }
-    var pos = Lim();
 
-    var sp = 0, //SectionPosition. эта координата двигает наш wrapper за счёт position left.
-        width = $(window).width(), //берём ширину экрана. нужно для определения правой границы.
-        wDelta = 100, //множитель скролла
-        wrapperWidth = 0,
-        _xDouble = $('.t-newsCardWrapper:last-child').index(),
-        x = 0; //ширина wrappera. нужна для определения правой границы.
-    console.log(_xDouble)
-
-    if (window.matchMedia('(min-width: 961px)').matches) {
-        $('body').on('mousewheel', hscroll);
-        $(document).on('keydown', hscroll);
-    function hscroll(event) {
-            if (window.matchMedia('(max-width: 960px)').matches) {
-                return;
-            }
-            var deltaY = 0,
-                section = $('.t-newsCardWrapper');
-            if (event.type.toLowerCase() == "mousewheel") {
-                if (event.deltaY > 0) {
-                    deltaY = 1;
-                } else {
-                    deltaY = -1
-                }
-            } else if ((event.type.toLowerCase() == "keydown") && (event.target.nodeName.toLowerCase() == 'body')) {
-                if (event.which == 38) {
-                    deltaY = 1;
-                } else if (event.which == 40) {
-                    deltaY = -1
-                } else {
-                    return;
-                }
-            }
-
-            if (!$('.b-popup').is(':visible')) {
-                sp -= deltaY * wDelta;
-            }
-
-            //определаем границы скролла
-            if (sp < 0) {
-                sp = 0;
-            } else if (sp > Lim()) {
-                sp = Lim();
-            }
-
-            //меняем позицию wrappera
-            $('.t-mainCardsHolder').css('transform', 'translate(' + -sp + 'px, 0)');
-
-        }
-
-    }
 });
