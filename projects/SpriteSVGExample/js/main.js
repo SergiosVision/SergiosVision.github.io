0036f579bef1c2b3582a362ac1ea8cb44712f662
SergiosVision <30858380+SergiosVision@users.noreply.github.com>
2018-07-31 15:49:49 +0700

    Remvoe Useless Projects

--- projects/SpriteSVGExample/js/main.js
+++
@@ -1,7 +0,0 @@
-$('.menu-icon2').mouseenter(function () {
-    console.log($('#logo-text'));
-    $('#logo-text path').css('fill', 'blue');
-});
-$('.menu-icon2').mouseout(function () {
-    $('#logo-text path').css('fill', '#fff');
-});
--- projects/gulpAndJade/app/libs/print.js
+++
@@ -1,5 +0,0 @@
-const getPrintBtn = document.querySelector('#print');
-
-getPrintBtn.addEventListener('click', function () {
-    window.print();
-}, false);
